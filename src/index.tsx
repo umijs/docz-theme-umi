4509eca231f44585c130e208f8269eb5abd7009a
focus7eleven <441002295@qq.com>
2019-04-14 19:12:05 +0800

    feat: 主题优化，增加 Bigfish 特性

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,16 @@ import { Global } from './styles/global'
 import { config } from './config'
 import { ThemeProvider } from './utils/theme'
 
+if (process.env.BIGFISH_VERSION) {
+  // 更换 favicon 为 Bigfish logo
+  const link: HTMLLinkElement = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'icon';
+  link.href = "https://gw-office.alipayobjects.com/basement_prod/7e9551ab-bbaf-48fc-9253-9470e6593d2f.png";
+  const head = document.getElementsByTagName('head')[0]
+  head.insertBefore(link, head.firstChild);
+}
+
 const Theme: SFC = ({ children }) => (
   <ThemeProvider>
     <Global />
